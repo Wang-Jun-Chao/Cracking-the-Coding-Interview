c38493ccc1ee6b211f1a11b54d829b29491d6395
shining-glory <shining-glory@qq.com>
2016-01-09 10:14:22 +0800

    commit

--- 0503-位运算-最接近的数/src/CloseNumber.java
+++ 0503-位运算-最接近的数/src/CloseNumber.java
@@ -7,78 +7,91 @@
  */
 public class CloseNumber {
     public int[] getCloseNumber(int x) {
-        int[] result = {0, 0};
+        return new int[]{getPrev(x), getNext(x)};
+    }
 
-        int one = 0;
-        int zero = 0;
-        int and = 1;
+    /**
+     * 找比n大的，二进制位数与n相同的最接近n的数
+     *
+     * @param n
+     * @return
+     */
+    public int getNext(int n) {
+        int c = n;
+        // 翻转最右边但非拖尾的0，把这个位置记作p
+        // c1为p右方连续l的个数，c0为p右方拖尾0的个数
+        int c0 = 0;
+        int c1 = 0;
 
-        // 从最低位置向最高位操作
-
-        // 找第一位0的位置
-        while ((x & and) == 1) {
-            zero++;
-            and <<= 1;
+        // 计算拖尾0的个数
+        while ((c & 1) == 0 && (c != 0)) {
+            c0++;
+            c >>= 1;
         }
-        and = 1;
-        // 找第一位1出现的位置
-        while ((x & and) == 0) {
-            one++;
-            and <<= 1;
-        }
 
-        // 产生第一个结果
-        int t = 0xFFFF_FFFF;
-        t ^= (1 << one);
-        result[0] = x | (1 << zero) & t;
-
-        // 这两位交换后会产生一个小于x的值
-        if (zero < one) {
-//            int t = 0xFFFF_FFFF;
-//            t ^= (1 << one);
-//            result[0] = x | (1 << zero) | t;
-
-            zero = one + 1;
-            and = (1 << zero);
-
-            // 找第二个0，第一个1还是使用
-            while ((x & and) == 1) {
-                zero++;
-                and <<= 1;
-            }
-
-            // 第二个结果
-            t = 0xFFFF_FFFF;
-            t ^= (1 << one);
-            result[1] = x | (1 << zero) & t;
-
-        }
-        // 这两位交换后会产生一个大于x的值
-        else {
-
-            one++;
-            zero = one;
-
-            // 找第二位0的位置
-            and = (1 << zero);
-            while ((x & and) == 1) {
-                zero++;
-                and <<= 1;
-            }
-            and = (1 << one);
-            // 找第一位1出现的位置
-            while ((x & and) == 0) {
-                one++;
-                and <<= 1;
-            }
-
-            // 第二个结果
-            t = 0xFFFF_FFFF;
-            t ^= (1 << one);
-            result[1] = x | (1 << zero) & t;
+        // 计算从拖尾0的前一个位置开始，向前找，连续的1的个数
+        while ((c & 1) == 1) {
+            c1++;
+            c >>= 1;
         }
 
 
-        return result;
+        // 错误：若n == 11...1100...00，那么就没有更大的数字。且1的个数相同
+        if (c0 + c1 == 31 || c0 + c1 == 0) {
+            return -1;
+        }
+
+        // 最右边、非拖尾零的位置
+        int p = c0 + c1;
+
+        // 翻转最右边、非拖尾零
+        n |= (1 << p);
+        // 将p右方的所有位清零
+        n &= ~((1 << p) - 1);
+        // 在右方插入(c1-1)个1
+        n |= (1 << (c1 - 1)) - 1;
+
+        return n;
+    }
+
+    /**
+     * 找比n小的，二进制位数与n相同的最接近n的数
+     *
+     * @param n
+     * @return
+     */
+    public int getPrev(int n) {
+        int c = n;
+        // 翻转最右边但非拖尾的1，把这个位置记作p
+        // c1为p右方拖尾l的个数，c0为p右方连续0的个数
+        int c0 = 0;
+        int c1 = 0;
+
+        // 找拖尾1的个数
+        while ((c & 1) == 1) {
+            c1++;
+            c >>= 1;
+        }
+
+        // 表明n的所有数的位数都是1
+        if (c == 0) {
+            return -1;
+        }
+
+        // 计算从拖尾1的前一个位置开始，向前找，连续的0的个数
+        while ((c & 1) == 0 && c != 0) {
+            c0++;
+            c >>= 1;
+        }
+
+        // 非拖一1的位置
+        int p = c0 + c1;
+        // 将n的后p+1位清零
+        n &= ((~0) << (p + 1));
+        // 掩码，最末c1+1位置1
+        int mask = (1 << (c1 + 1)) - 1;
+        n |= mask << (c0 - 1);
+
+        return n;
     }
 }
--- 0503-位运算-最接近的数/src/Main.java
+++ 0503-位运算-最接近的数/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Author: 王俊超
  * Date: 2016-01-07 14:15
@@ -7,8 +9,23 @@
  */
 public class Main {
     public static void main(String[] args) {
-        CloseNumber cn =new CloseNumber();
-        int[] r = cn.getCloseNumber(2);
-        System.out.println(r[0] + ", " + r[1]);
+        CloseNumber cn = new CloseNumber();
+
+//        Random r = new Random();
+//        for (int i = 0; i < 20; i++) {
+//            int v = r.nextInt(99998) + 2;
+//            int[] rs = cn.getCloseNumber(v);
+//            System.out.println(v + ": " + rs[0] + ", " + rs[1]);
+//            System.out.println(Integer.toBinaryString(v));
+//            System.out.println(Integer.toBinaryString(rs[0]));
+//            System.out.println(Integer.toBinaryString(rs[1]));
+//        }
+
+
+        int[] rs = cn.getCloseNumber(76351);
+        System.out.println(76351 + ": " + rs[0] + ", " + rs[1]);
+        System.out.println(Integer.toBinaryString(76351));
+        System.out.println(Integer.toBinaryString(rs[0]));
+        System.out.println(Integer.toBinaryString(rs[1]));
     }
 }
