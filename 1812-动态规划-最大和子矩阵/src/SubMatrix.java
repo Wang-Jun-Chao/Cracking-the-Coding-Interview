487a7be6be83b89b9ed723fcfdf0b3046b7b57ce
user <oracleer@foxmail.com>
2016-01-12 14:05:24 +0800

    commit

--- 1812-动态规划-最大和子矩阵/src/SubMatrix.java
+++ 1812-动态规划-最大和子矩阵/src/SubMatrix.java
@@ -8,19 +8,34 @@
 public class SubMatrix {
     public int sumOfSubMatrix(int[][] mat, int n) {
 
+        // 矩阵的行数
         int rowCount = mat.length;
+        // 矩阵的列数
         int colCount = mat[0].length;
 
+        // 部分和
         int[] partialSum = new int[colCount];
+        // 最大值
         int maxSum = Integer.MIN_VALUE;
 
+        // rowStart限制开始处理的行
         for (int rowStart = 0; rowStart < rowCount; rowStart++) {
+            // 对部分知清零
             clear(partialSum);
+
+            // 下面对[rowStart,...,rowEnd]列表进行处理
             for (int rowEnd = rowStart; rowEnd < rowCount; rowEnd++) {
+                // 对所有的列进行处理
                 for (int col = 0; col < colCount; col++) {
+                    // partialSum[col]的值是mat[rowStart...rowEnd][col]行累加的值
                     partialSum[col] += mat[rowEnd][col];
                 }
 
+                // 在此处partialSum[rowStart,...,rowEnd]数组就是
+                // mat[rowStart,...,rowEnd][0,...,colCount-1]就对列的累加
+
+                // 此处的tempMaxSum就是mat[rowStart,...,rowEnd][x,...,y]的某个值
+                // 其中0<=x<=y<=(colCount-1)
                 int tempMaxSum = maxSubArray(partialSum);
                 if (tempMaxSum > maxSum) {
                     maxSum = tempMaxSum;
---
+++ 1813-动态规划-最大字母矩阵/src/AlphaMatrix.java
@@ -0,0 +1,9 @@
+/**
+ * Author: 王俊超
+ * Date: 2016-01-12 14:05
+ * CSDN: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/Wang-Jun-Chao
+ * All Rights Reserved !!!
+ */
+public class AlphaMatrix {
+}
